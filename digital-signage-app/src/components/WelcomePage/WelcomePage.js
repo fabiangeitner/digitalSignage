f0478abdff6006a7572d372adbee372e77464364
fabiangeitner <fabian-geitner@web.de>
2021-12-29 12:13:51 +0100

    finished welcome-page

--- digital-signage-app/src/components/WelcomePage/WelcomePage.js
+++ digital-signage-app/src/components/WelcomePage/WelcomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
-import WelcomePageCss from "./WelcomePage.css";
+import "./WelcomePage.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import welcome from "../../Assets/WelcomePage.png";
@@ -10,18 +11,22 @@ export const WelcomePage = () => {
     <>
       <div className="containerTest">
         <div class="header">
-          <h1 class="py-4">STORYTIME</h1>
+          <span class="header-headline">STORYTIME</span>
         </div>
-        <h2 class="pt-3 pb-1">WILLKOMMEN</h2>
-        <div class="content">
+        <div className="content">
+          <h2 class="pt-4 pb-1">WILLKOMMEN</h2>
           <p>So funktioniert's:</p>
-          <img className="welcome"
-                src={welcome}
-                alt="Welcome Erläuterung"
-              />
-          <h3 class="py-2">Nutzen Sie ihr Smartphone zur Steuerung!</h3>
+          <Link to="/overview">
+            <img
+              className="welcomeImg my-5"
+              src={welcome}
+              alt="Welcome Erläuterung"
+            />
+          </Link>
+
+          <h3 class="py-3">Nutzen Sie ihr Smartphone zur Steuerung!</h3>
         </div>
-        <div class="py-5 footer"></div>
+        <div class="footer"></div>
       </div>
     </>
   );
