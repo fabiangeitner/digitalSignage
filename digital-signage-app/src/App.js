1d49ac3412363e20ca8a1d7b42dff2cc6c74e31d
fabiangeitner <fabian-geitner@web.de>
2021-12-29 10:14:54 +0100

    Create Component

--- digital-signage-app/src/App.js
+++ digital-signage-app/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { WelcomePage } from "./components/WelcomePage";
 import { Slideshow } from "./components/Slideshow/Slider";
 import { TestComponent } from "./components/TestComponent";
 
@@ -9,6 +11,7 @@ function App() {
         {/* <Route path="/" element={<Slider slides={SliderData} />} /> */}
         <Route path="test" element={<TestComponent />} />
         <Route path="/" element={<Slideshow />} />
+        <Route path="welcome" element={<WelcomePage />} />
       </Routes>
     </div>
   );
