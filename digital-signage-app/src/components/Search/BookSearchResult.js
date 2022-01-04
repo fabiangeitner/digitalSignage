91c2fd5f1c148e4cccad2a7bd5aee4c67acac2c7
MartinPanaget <m.panaget@t-online.de>
2022-01-04 13:10:51 +0100

    serach finished

---
+++ digital-signage-app/src/components/Search/BookSearchResult.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import { Header } from "../Header/Header";
+
+export const BookSearchResult = () => {
+    return (
+      <>
+        <Header></Header>
+      </>
+    );
+};
