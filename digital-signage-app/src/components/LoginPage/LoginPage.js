4192c5c237235c4c6f4fa71ed31f12961111ae77
milli99 <milli.fiorino@web.de>
2022-01-13 19:52:05 +0100

    added all elements

--- digital-signage-app/src/components/LoginPage/LoginPage.js
+++ digital-signage-app/src/components/LoginPage/LoginPage.js
@@ -2,24 +2,38 @@ import React from 'react'
 import styled from "styled-components";
 
 const Headline = styled.div`
-  h2 {
-    font-size: 20px;
+  h1 {
+    font-size: 28px;
     font-family: sofia-pro, sans-serif;
-    font-weight: 700;
+    font-weight: 400;
     color: #000;
     text-align: center;
   }
 `;
 
 const Login = styled.div`
-  h3 {
-    font-size: 20px;
+
+  h2 {
+    font-size: 19px;
     font-family: sofia-pro, sans-serif;
     font-weight: 700;
     color: #000;
+    margin-bottom: 20px;
   }
-
-  input {
+  h3 {
+    font-size: 13px;
+    font-family: sofia-pro, sans-serif;
+    font-weight: 600;
+    color: #23645D;
+    margin-bottom: 30px;
+  }
+  input{
+    width: 80%;
+    padding: 10px 20px;
+    margin: 8px 0;
+    border-radius: 4px;
+    border: 1px solid #707070;
+    color: #707070;
     
   }
   p {
@@ -28,22 +42,72 @@ const Login = styled.div`
     font-weight: 300;
     color: #707070;
     text-align: center;
+    margin-bottom: 50px;
+  }
+  div {
+    width: 80%;
+    background-color: #FF772F;
+    font-family: sofia-pro, sans-serif;
+    text-align: center;
+    color: white;
+    padding: 10px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
   }
 `;
 
+const Register = styled.div`
+    h2 {
+        font-size: 19px;
+        font-family: sofia-pro, sans-serif;
+        font-weight: 700;
+        color: #000;
+        margin-bottom: 20px;
+    }
+    div {
+        width: 80%;
+        background-color: #FF772F;
+        font-family: sofia-pro, sans-serif;
+        text-align: center;
+        color: white;
+        padding: 10px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+      p {
+        font-size: 19px;
+        font-family: sofia-pro, sans-serif;
+        font-weight: 300;
+        color: #FF772F;
+        text-align: center;
+        margin-bottom: 50px;
+      }
+`;
 export const LoginPage = () => {
     return (
         <div>
-            <Headline className="px-3 py-2">
-                <h2 className="my-3">Sign in</h2>
+            <Headline>
+                <h1 className="my-3">Sign in</h1>
             </Headline>
-            <Login>
-                <h3>Sind Sie bereits Kunde?</h3>
-                <input type="text"></input>
-                <input type="text"></input>
-                <button>Anmelden</button>
-                <p>Mit der Anmeldung erkläre stimme ich den AGBs <br/> und der Datenschutzerklärung zu.</p>
+            <Login> 
+                <form > 
+                    <h2 className="px-4 py-2">Sind Sie bereits Kunde?</h2>
+                    <input type="email" className="form-control" placeholder="E-Mail-Adresse"></input>
+                    <input type="passwort" className="form-control" placeholder="Passwort"></input>
+                    <h3 className="px-3 py-2">Passwort vergessen?</h3>
+                    <div className="mx-auto">Anmelden</div>
+                    <p>Mit der Anmeldung erkläre stimme ich den AGBs <br/> und der Datenschutzerklärung zu.</p>
+                </form>
             </Login>
+            <Register>
+                <h2 className="px-3 py-2">Ein Kundenkonto lohnt sich!</h2>
+                <div className="mx-auto">Jetzt Konto anlegen</div>
+                <p>Als Gast fortfahren</p>
+            </Register>
         </div>
     )
 }
