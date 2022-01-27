3064e441dbd787e9a067142463dde54630093eb5
milli99 <milli.fiorino@web.de>
2022-01-27 13:50:25 +0100

    adjusted cartItems

--- digital-signage-app/src/components/Cart/CartItems.js
+++ digital-signage-app/src/components/Cart/CartItems.js
@@ -1,24 +1,12 @@
-import item1 from "../../Assets/Books/item1.png";
-import item2 from "../../Assets/Books/item2.png";
-import item3 from "../../Assets/Books/item3.png";
+import item1 from "../../Assets/Books/buch4.jpeg";
+
 
 export const Items = [
     {
-      headline: "If I stay",
-      author: "Gayle Forman",
+      headline: "Ein ganzes halbes Jahr",
+      author: "Jojo Moyes",
       price: "15€",
       cover: item1
     },
-    {
-      headline: "Mord im alten Pfarrhaus",
-      author: "Jill Mcgown",
-      price: "10,95€",
-      cover: item2
-    },
-    {
-      headline: "It ends with us",
-      author: "Colleen Hoover",
-      price: "18,99€",
-      cover: item3
-    },
+    
   ];
