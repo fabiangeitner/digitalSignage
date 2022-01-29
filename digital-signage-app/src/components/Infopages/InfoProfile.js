e7edb2a43b730c7c4e5b6a579901e651f6de7074
fabiangeitner <fabian-geitner@web.de>
2022-01-29 21:53:35 +0100

    Change_Screens

--- digital-signage-app/src/components/Infopages/InfoProfile.js
+++ digital-signage-app/src/components/Infopages/InfoProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Header } from "../Header/Header";
-import Smartphone from "../../Assets/Smartphone.png";
+import Smartphone from "../../Assets/Smartphone2.png";
 
 const InfoProfileWrapper = styled.div`
   height: 92vh;
